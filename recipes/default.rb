1435b86ff22d9d61f604a415378c8b0371522f04
amilyviju <amily.viju@reancloud.com>
2017-09-13 14:32:26 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,51 +6,6 @@
 execute 'install' do
   command 'yum -y install ipa-client'
 end
-execute 'echo' do
-  command 'echo 10.0.1.113 awsfreipa.rean.local awsfreipa >> /etc/hosts'
-end
-execute 'yum' do
-  command 'yum -y install firewalld'
-end
-execute 'start' do
-  command 'systemctl start firewalld'
-end
-execute 'enable' do
-  command 'systemctl enable firewalld'
-end
-execute 'yum' do
-  command 'yum install firewalld'
-end
-execute 'set-hostname' do
-  command 'hostnamectl set-hostname ipaclient02.rean.local --static'
-end
-execute 'zone' do
-  command 'firewall-cmd --permanent --zone=public --change-interface=eth0'
-end
-execute 'reload' do
-  command 'firewall-cmd --reload'
-end
-execute 'port1' do
-  command 'firewall-cmd --zone=public --add-port=88/tcp'
-end
-execute 'port2' do
-  command 'firewall-cmd --zone=public --add-port=80/tcp'
-end
-execute 'port3' do
-  command 'firewall-cmd --zone=public --add-port=389/tcp'
-end
-execute 'port4' do
-  command 'firewall-cmd --zone=public --add-port=88/udp'
-end
-execute 'port5' do
-  command 'firewall-cmd --zone=public --add-port=464/tcp'
-end
-execute 'port6' do
-  command 'firewall-cmd --zone=public --add-port=464/udp'
-end
-execute 'port7' do
-  command 'firewall-cmd --zone=public --add-port=123/udp'
-end
 execute 'installation' do
   command 'ipa-client-install -U --domain=rean.local --force-ntpd  --force -p admin -w password --server=awsfreipa.rean.local'
 end
